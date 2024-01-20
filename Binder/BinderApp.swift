36a51195067bd63c54919f75f01775a3b84cae32
Samara <97859147+ssalggnikool@users.noreply.github.com>
2024-01-19 23:27:32 -0800

    Initial Commit

---
+++ Binder/BinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinderApp.swift
+//  Binder
+//
+//  Created by samara on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Binder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Binder
+//
+//  Created by samara on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
